a727f5ce743b585d5b657d753bd7290b6c16a3ec
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-09-09 17:57:43 -0400

    router scaffolding

--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -15,9 +15,10 @@ eventRouter.route('/')
     res.end('Will send all events to you');
 })
 .post((req, res) => {
-    const eventName = req.body.name; // This is how we get information out of the request. req.body.whatever-will-be-submitted
-    const eventDescription = req.body.description;
-    res.end(`Will update the events with the following:\nEvent name: ${eventName}\nEvent description: ${eventDescription}`)
+    const name = req.body.name; // This is how we get information out of the request. req.body.whatever-will-be-submitted
+    const description = req.body.description;
+    const date = req.body.date;
+    res.end(`Will update the events with the following:\nEvent name: ${name}\nEvent description: ${description}\nEvent date: ${Date(date)}`)
 })
 .put((req, res) => {
     res.statusCode = 403;
@@ -26,5 +27,24 @@ eventRouter.route('/')
 .delete((req, res) => {
     res.end('Deleting all events');
 });
+
+// Route = /events/:eventId for targeting specific events. put/delete should require authentication
+eventRouter.route('/:eventId')
+.get((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json')
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /events/' + req.params.eventId)
+})
+.put((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json')
+})
+.delete((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json')
+})
 
 module.exports = eventRouter;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,28 +1,45 @@
 const express = require('express');
-
 const userRouter = express.Router();
 
 
 userRouter.route('/')
-.all((req, res, next) => {
+.get((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
     res.end('Will send all users to you');
 })
-.post((req, res) => {
+.post((req, res, next) => {
+    res.statusCode = 200;
     const userName = req.body.name;
     const userDescription = req.body.description;
     res.end(`Will update the users with the following:\nUser name: ${userName}\nUser description: ${userDescription}`)
 })
-.put((req, res) => {
+.put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /users')
 })
-.delete((req, res) => {
+.delete((req, res, next) => {
+    res.statusCode = 200;
     res.end('Deleting all users');
+});
+
+// Routes for dealing with single user
+userRouter.route('/:userId')
+.get((req, res, next) => {
+    res.statusCode = 200;
+    res.end('Will send a single user to you');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /users')
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    // Update Single document defined by userId
+    res.end('PUT operation not supported on /users/' + req.params.userId)
+})
+.delete((req, res, next) => {
+    res.statusCode = 200;
+    res.end('Delete a single user with id: ' + req.params.userId);
 });
 
 module.exports = userRouter;
