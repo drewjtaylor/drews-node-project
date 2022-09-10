3bc98d17e2fb5d0bad27f8a2ed550079a46c687f
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-09-10 14:54:52 -0400

    authentication module

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'mongoUrl' : 'mongodb://localhost:27017/formationdb'
+    'mongoUrl' : 'mongodb://localhost:27017/formationdb',
+    'secretKey': 'dc4ejqc344fdasdfasdf3fh43nfqvjn23rf2309j1fad'
 }
