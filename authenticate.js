17c8382b9d39510f6751012cd8a048f47577b491
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-09-10 14:54:32 -0400

    authentication module

---
+++ authenticate.js
@@ -0,0 +1,52 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const jwt = require('jsonwebtoken');
+const config = require('./config.js');
+
+const User = require('./models/User');
+
+// What does this do?  Do we still need it after jwt?    Where does authenticate method come from?
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = user => {
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+};
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        return next()
+    } else {
+        err = new Error('You are not authorized for this operation!');
+        err.statusCode = 403;
+        return next(err)
+    }
+};
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(
+    new JwtStrategy(
+        opts,
+        (jwt_payload, done) => {
+            User.findOne({_id: jwt_payload._id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                } else if (user) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            })
+        }
+    )
+)
