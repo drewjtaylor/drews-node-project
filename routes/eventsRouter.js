b993028f2b58e87e6865eeb24e77a3856b8e85a3
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-09-10 14:55:44 -0400

    GET /events

--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -1,20 +1,21 @@
 const express = require('express');
-
 const eventRouter = express.Router();
+const authenticate = require('../authenticate');
+const Event = require('../models/Event');
 
 
-// Routing method acts as a catch-all for any HTTP verb
-// As with all routing methods, takes 2 parameters(path, function-to-do-when-called)
 eventRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();  // passes control to the next routing method after this one. Otherwise it would stop here, and not go further
+.get((req, res) => {
+    Event.find()
+    // .populate('creator')
+    .then(events => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(events)
+    })
+    .catch(err => next(err))
 })
-.get((req, res) => { //"next" not included because there should be no more instructions after this step)
-    res.end('Will send all events to you');
-})
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     const name = req.body.name; // This is how we get information out of the request. req.body.whatever-will-be-submitted
     const description = req.body.description;
     const date = req.body.date;
