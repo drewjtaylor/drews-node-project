c41878455bc19c827f8cff327614f7e2f4aad363
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-09-11 00:10:41 -0400

    post new user

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const userRouter = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const authenticate = require('../authenticate');
+const User = require('../models/User');
 
 
 userRouter.route('/')
 .get((req, res, next) => {
-    res.statusCode = 200;
-    res.end('Will send all users to you');
+    User.find()
+    .then(users => {
+        if (!users) {
+            return
+        };
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users)
+    })
+    .catch(err => next(err))
 })
 .post((req, res, next) => {
-    res.statusCode = 200;
-    const userName = req.body.name;
-    const userDescription = req.body.description;
-    res.end(`Will update the users with the following:\nUser name: ${userName}\nUser description: ${userDescription}`)
+    User.register(
+        new User({username: req.body.username, email: req.body.email}),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                console.log('mark3');
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({err: err});
+                console.log('There was an error registering new user');
+                return next(err)
+            } else {
+                if (req.body.firstName) {
+                    user.firstName = req.body.firstName
+                };
+                if (req.body.lastName) {
+                    user.lastName = req.body.lastName
+                };
+                user.save(err => {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({err: err});
+                        return next(err)
+                    };
+                    passport.authenticate('local')(req, res, () => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({success: true, status: 'Registration successful'});
+                    })
+                })
+            }
+        }
+    )
 })
-.put((req, res, next) => {
+.put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /users')
 })
@@ -24,20 +66,39 @@ userRouter.route('/')
 
 // Routes for dealing with single user
 userRouter.route('/:userId')
-.get((req, res, next) => {
+.get(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
     res.statusCode = 200;
     res.end('Will send a single user to you');
 })
 .post((req, res, next) => {
     res.statusCode = 403;
-    res.end('POST operation not supported on /users')
+    res.end('POST operation not supported on /users/' + req.params.userId)
 })
-.put((req, res, next) => {
-    res.statusCode = 403;
-    // Update Single document defined by userId
-    res.end('PUT operation not supported on /users/' + req.params.userId)
+.put(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+    User.findByIdAndUpdate(req.params.userId, 
+        {
+            $set: req.body
+        },
+    {
+        new: true
+    })
+    .then(user => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(user);
+    })
+    .catch(err => next(err))
 })
-.delete((req, res, next) => {
+.delete(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
     res.statusCode = 200;
     res.end('Delete a single user with id: ' + req.params.userId);
 });
