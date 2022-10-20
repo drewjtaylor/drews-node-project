23733c5db18928dcff80e9af1bc279bb2e79765f
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-10-20 15:08:52 -0400

    Can add multiple users as attendees, checks if already attending

--- MongoDBtests/dbTest.test.js
+++ MongoDBtests/dbTest.test.js
@@ -26,19 +26,21 @@ describe('insert', () => {
 
         // Clear previous entry if it failed to delete before
         await users.deleteOne({_id: 'some-user-id'});
-
+        
         const mockUser = {
             _id: 'some-user-id',
             email: 'jest-test-email@something.com',
             firstName: 'FirstJestName',
             lastName: 'LastJestName'
         }
-
-
-
+        
+        
+        
         await users.insertOne(mockUser);
         const insertedUser = await users.findOne({_id: 'some-user-id'});
         expect(insertedUser).toEqual(mockUser);
+        
+        await users.deleteOne({_id: 'some-user-id'});
     })
 
 
--- endpointTests/events.test.js
+++ endpointTests/events.test.js
@@ -4,19 +4,27 @@ const app = require('../app')
 
 describe('GET /events', () => {
     it('should get all events', async () => {
-        const response = await request(app)
+        const response = await request('https://127')
             .get('/events');
-            // await expect(response.statusCode).toBe(200) // If redirected to 308, test fails
-            await expect([200, 308]).toContain(response.statusCode) // Allows for 200 or 308
+            await expect(response.statusCode).toBe(200) // If redirected to 308, test fails
+            await expect([200, 308]).toContain(response.statusCode) // Allows for 200 or 308, but because everything redirects, this is no good
         })
 
-        it('Should get one event from 12/12/26', async () => {
-            const response = await request(app)
+    it('Should get one event from 12/12/26', async () => {
+        const response = await request(app)
             .get('/events')
             .send({ startDate: '2022-12-26' })
-            
-            // await expect(response.statusCode).toBe(200) // If redirected to 308, test fails
-            await expect([200, 308]).toContain(response.statusCode) // Allows for 200 or 308 response
-        console.log(`The response is:\n${JSON.stringify(response, null, 2)}`)
+
+            await expect(response.statusCode).toBe(200) // If redirected to 308, test fails
+            // await expect([200, 308]).toContain(response.statusCode) // Allows for 200 or 308 response
+    })
+})
+
+describe('USERS', () => {
+    it('should sign in Jest test user', async () => {
+        const response = await request(app)
+            .post('/users/login')
+            .send({username: "admin", password: "password"})
+            await expect([200, 308]).toContain(response.statusCode)
     })
 })
